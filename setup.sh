46e5f35e228f24b106a485c83171cfae80f35cfe
joaquimcardeira <joaquim.cardeira@gmail.com>
2025-09-08 13:24:04 +0100

    start multi platform

--- setup.sh
+++ setup.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-#sudo apt-get update
 mkdir nrf-sdk || echo ">>> nrf-sdk directory already exists, skipping creation."
 
-wget -q https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-linux.tar.bz2
+# Detect platform and download appropriate toolchain
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-mac.tar.bz2"
+    TOOLCHAIN_FILE="gcc-arm-none-eabi-6-2017-q2-update-mac.tar.bz2"
+else
+    TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-linux.tar.bz2"
+    TOOLCHAIN_FILE="gcc-arm-none-eabi-6-2017-q2-update-linux.tar.bz2"
+fi
+
+echo ">>> Downloading ARM toolchain..."
+wget -q "$TOOLCHAIN_URL"
 
 echo ">>> Extracting toolchain to nrf-sdk directory..."
-tar -xjf gcc-arm-none-eabi-6-2017-q2-update-linux.tar.bz2 -C nrf-sdk
+tar -xjf "$TOOLCHAIN_FILE" -C nrf-sdk
 
 echo ">>> Downloading nRF5 SDK 12.3.0..."
 wget -q https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v12.x.x/nRF5_SDK_12.3.0_d7731ad.zip
@@ -21,15 +30,20 @@ wget -q https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v15.x.x/nRF5_SDK_15.3
 echo ">>> Extracting nRF5 SDK 15.3.0 to nrf-sdk directory..."
 unzip -q nRF5_SDK_15.3.0_59ac345.zip -d nrf-sdk
 
-sed -i 's|GNU_INSTALL_ROOT ?= .*|GNU_INSTALL_ROOT ?= ../../nrf-sdk/gcc-arm-none-eabi-6-2017-q2-update/bin/|' \
+sed -i.bak 's|GNU_INSTALL_ROOT ?= .*|GNU_INSTALL_ROOT ?= ../../nrf-sdk/gcc-arm-none-eabi-6-2017-q2-update/bin/|' \
 nrf-sdk/nRF5_SDK_15.3.0_59ac345/components/toolchain/gcc/Makefile.posix
-
+rm -f Makefile.bak 
 
 
 echo ">>> Installing Nordic nRF Command Line Tools (v10.24.2)..."
-wget -q https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-command-line-tools/sw/versions-10-x-x/10-24-2/nrf-command-line-tools_10.24.2_amd64.deb
-sudo apt-get update
-sudo apt-get install -y ./nrf-command-line-tools_10.24.2_amd64.deb
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo ">>> On macOS, please install nRF Command Line Tools manually from:"
+    echo "    https://www.nordicsemi.com/Products/Development-tools/nRF-Command-Line-Tools/Download"
+else
+    wget -q https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-command-line-tools/sw/versions-10-x-x/10-24-2/nrf-command-line-tools_10.24.2_amd64.deb
+    sudo apt-get update
+    sudo apt-get install -y ./nrf-command-line-tools_10.24.2_amd64.deb
+fi
 
 python -m venv venv
 source venv/bin/activate
