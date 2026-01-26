28f7250305c7e51512387a75617e5adfed28917d
joaquimcardeira <joaquim.cardeira@gmail.com>
2026-01-26 16:24:19 +0000

    setup.sh

---
+++ tools/setup.sh
@@ -0,0 +1,9 @@
+brew install arm-none-eabi-gcc
+brew install openocd
+brew install nrfutil
+
+echo ">>> Downloading nRF5 SDK 12.3.0..."
+wget -q https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v12.x.x/nRF5_SDK_12.3.0_d7731ad.zip
+
+echo ">>> Extracting nRF5 SDK to nrf-sdk directory..."
+unzip -q nRF5_SDK_12.3.0_d7731ad.zip -d ../nrf-sdk
